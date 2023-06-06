2e516f62b182551f8f13084a6e142dfd30e892bc
Parag More <paragmore306@gmail.com>
2023-06-06 11:01:13 +0530

    supplier lsit and details

---
+++ src/app/pages/suppliers/suppliers-list/suppliers-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suppliers-list',
+  templateUrl: './suppliers-list.component.html',
+  styleUrls: ['./suppliers-list.component.scss'],
+  standalone: true,
+})
+export class SuppliersListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
