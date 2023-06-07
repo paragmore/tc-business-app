5b1e8e142d937462391ef989a9252858cbcdaf51
Parag More <paragmore306@gmail.com>
2023-06-07 17:44:05 +0530

    entries ledger list

--- src/app/core/components/entries-ledger-list/entries-ledger-list.component.ts
+++ src/app/core/components/entries-ledger-list/entries-ledger-list.component.ts
@@ -1,14 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { ScreenModel } from 'src/app/store/models/screen.models';
+import { AppState } from 'src/app/store/models/state.model';
+import { HyphenPipe } from '../../pipes/hyphen.pipe';
+import { PaginationComponentComponent } from '../pagination-component/pagination-component.component';
 
 @Component({
   selector: 'app-entries-ledger-list',
   templateUrl: './entries-ledger-list.component.html',
   styleUrls: ['./entries-ledger-list.component.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+    HyphenPipe,
+    PaginationComponentComponent,
+  ],
 })
-export class EntriesLedgerListComponent  implements OnInit {
+export class EntriesLedgerListComponent implements OnInit {
+  @Input() ledgerData!: EntriesLedgerDataI;
+  isLoading = false;
+  public screenState$: Observable<ScreenModel> | undefined;
+  isMobile = false;
+  constructor(private store: Store<AppState>) {}
 
-  constructor() { }
+  ngOnInit() {
+    this.screenState$ = this.store.select((store) => store.screen);
+    this.screenState$.subscribe((screen) => (this.isMobile = screen.isMobile));
+  }
+}
 
-  ngOnInit() {}
+export interface EntriesLedgerItemColI {
+  text: string;
+  subtext?: string;
+  color?: string;
+}
+export interface EntriesLedgerItemI {
+  col1: EntriesLedgerItemColI;
+  col2: EntriesLedgerItemColI;
+  col3: EntriesLedgerItemColI;
+  onClick: (ledger: EntriesLedgerItemI) => void;
+  openItemDetailsPage: (ledger: EntriesLedgerItemI) => void;
+}
 
+export interface EntriesLedgerDataI {
+  ledgerItems: EntriesLedgerItemI[];
+  onSort: () => void;
+  isLoading: boolean;
+  currentPage: number;
+  totalPages: number;
+  goToPage: (event: any) => void;
+  changePageSize: (event: any) => void;
+  col1Title: string;
+  col2Title: string;
+  col3Title: string;
 }
--- src/app/pages/customers/customer-details/customer-details.component.ts
+++ src/app/pages/customers/customer-details/customer-details.component.ts
@@ -5,13 +5,23 @@ import {
   BasicPartyDetailsComponent,
   BasicPartyDetailsInputI,
 } from 'src/app/basic-party-details/basic-party-details.component';
+import {
+  EntriesLedgerDataI,
+  EntriesLedgerItemI,
+  EntriesLedgerListComponent,
+} from 'src/app/core/components/entries-ledger-list/entries-ledger-list.component';
 
 @Component({
   selector: 'app-customer-details',
   templateUrl: './customer-details.component.html',
   styleUrls: ['./customer-details.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule, BasicPartyDetailsComponent],
+  imports: [
+    CommonModule,
+    IonicModule,
+    BasicPartyDetailsComponent,
+    EntriesLedgerListComponent,
+  ],
 })
 export class CustomerDetailsComponent implements OnInit {
   dummyPartyDetails: BasicPartyDetailsInputI = {
@@ -25,6 +35,57 @@ export class CustomerDetailsComponent implements OnInit {
       prefix: "You'll get",
     },
   };
+
+  // Define dummy data
+  dummyData: EntriesLedgerDataI = {
+    ledgerItems: [
+      {
+        col1: {
+          text: 'Item 1',
+          subtext: 'Subtext 1',
+          color: 'red',
+        },
+        col2: {
+          text: 'Item 2',
+          subtext: 'Subtext 2',
+          color: 'blue',
+        },
+        col3: {
+          text: 'Item 3',
+          subtext: 'Subtext 3',
+          color: 'green',
+        },
+        onClick: (ledger: EntriesLedgerItemI) => {
+          // Handle onClick logic here
+          console.log('Clicked on ledger item:', ledger);
+        },
+        openItemDetailsPage: (ledger: EntriesLedgerItemI) => {
+          // Handle openItemDetailsPage logic here
+          console.log('Opened item details page for ledger:', ledger);
+        },
+      },
+      // Add more ledger items as needed
+    ],
+    onSort: () => {
+      // Handle onSort logic here
+      console.log('Sorting...');
+    },
+    isLoading: false,
+    currentPage: 1,
+    totalPages: 5,
+    goToPage: (event: any) => {
+      // Handle goToPage logic here
+      console.log('Go to page:', event.target.value);
+    },
+    changePageSize: (event: any) => {
+      // Handle changePageSize logic here
+      console.log('Change page size:', event.target.value);
+    },
+    col1Title: 'Column 1',
+    col2Title: 'Column 2',
+    col3Title: 'Column 3',
+  };
+
   constructor() {}
 
   ngOnInit() {}
