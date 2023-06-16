ef347a27a34b543d499523bd4537e21b2a703394
Parag More <paragmore306@gmail.com>
2023-06-16 10:31:33 +0530

    discount list card

--- src/app/pages/items/discounts-list/discounts-list.component.ts
+++ src/app/pages/items/discounts-list/discounts-list.component.ts
@@ -2,10 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import {
   DiscountI,
   VariantI,
 } from 'src/app/core/services/products/products.service';
+import { ScreenModel } from 'src/app/store/models/screen.models';
+import { AppState } from 'src/app/store/models/state.model';
 
 @Component({
   selector: 'app-discounts-list',
@@ -17,7 +21,13 @@ import {
 export class DiscountsListComponent implements OnInit {
   @Input() discounts?: DiscountI[];
   @Input() readonly = false;
-  constructor() {}
+  isMobile = false;
 
-  ngOnInit() {}
+  screenState$: Observable<ScreenModel> | undefined;
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.screenState$ = this.store.select((store) => store.screen);
+    this.screenState$.subscribe((screen) => (this.isMobile = screen.isMobile));
+  }
 }
