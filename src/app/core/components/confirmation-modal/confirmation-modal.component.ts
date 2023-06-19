b73b7c1bf20f943c9ac7b136878109d379747d3f
Parag More <paragmore306@gmail.com>
2023-06-19 19:00:44 +0530

    confirmation modal ui

--- src/app/core/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/core/components/confirmation-modal/confirmation-modal.component.ts
@@ -1,16 +1,49 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { DialogHeaderComponent } from '../dialog-header/dialog-header.component';
 
 @Component({
   selector: 'app-confirmation-modal',
   templateUrl: './confirmation-modal.component.html',
   styleUrls: ['./confirmation-modal.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, CommonModule, DialogHeaderComponent],
 })
 export class ConfirmationModalComponent implements OnInit {
-  constructor() {}
+  @Input() confirmationModalInput!: ConfirmationModalInputI;
+  constructor(private modalController: ModalController) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log(this.confirmationModalInput);
+  }
+
+  onConfirmClicked() {
+    this.confirmationModalInput.ctaButton.onClick();
+  }
+
+  onCloseConfirmationModal = () => {
+    this.modalController.dismiss();
+  };
+}
+
+export interface ConfirmationEventI {
+  confirm: boolean;
+}
+
+export interface ConfirmationModalInputI {
+  headerTitle: string;
+  body: {
+    title: string;
+    icon: {
+      name: string;
+      class?: string;
+    };
+    subText: string;
+  };
+  ctaButton: {
+    text: string;
+    class?: string;
+    onClick: () => void;
+  };
 }
