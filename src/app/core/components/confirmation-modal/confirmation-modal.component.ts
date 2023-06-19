d0439488388894b927699a1dae9070bdceed2354
Parag More <paragmore306@gmail.com>
2023-06-19 17:50:10 +0530

    confirmation modal init

---
+++ src/app/core/components/confirmation-modal/confirmation-modal.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-confirmation-modal',
+  templateUrl: './confirmation-modal.component.html',
+  styleUrls: ['./confirmation-modal.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
+})
+export class ConfirmationModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
