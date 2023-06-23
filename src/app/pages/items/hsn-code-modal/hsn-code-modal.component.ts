c6f4c8fb24a0f27921842d7ba1c357ae7af35626
Parag More <paragmore306@gmail.com>
2023-06-23 18:08:20 +0530

    hsn sac modal listing

--- src/app/pages/items/hsn-code-modal/hsn-code-modal.component.ts
+++ src/app/pages/items/hsn-code-modal/hsn-code-modal.component.ts
@@ -1,8 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { DialogHeaderComponent } from 'src/app/core/components/dialog-header/dialog-header.component';
+import {
+  PartiesService,
+  PartyTypeEnum,
+} from 'src/app/core/services/parties/parties.service';
+import {
+  ItemTypeEnum,
+  ProductsService,
+  SortOrder,
+} from 'src/app/core/services/products/products.service';
 
 @Component({
   selector: 'app-hsn-code-modal',
@@ -12,37 +21,107 @@ import { DialogHeaderComponent } from 'src/app/core/components/dialog-header/dia
   imports: [IonicModule, CommonModule, DialogHeaderComponent, FormsModule],
 })
 export class HsnCodeModalComponent implements OnInit {
-  itemList: string[] = [
-    'GST @ 0%',
-    'GST @ 0.1%',
-    'GST @ 0.25%',
-    'GST @ 3%',
-    'GST @ 5%',
-    'GST @ 6%',
-    'GST @ 7.5%',
-    'GST @ 12%',
-    'GST @ 18%',
-    'GST @ 28%',
-  ];
+  @Input() itemType!: ItemTypeEnum;
+  hsnCodes: HSNSACCodeI[] = [];
   newItem: string | undefined;
+  currentPage = 1;
+  totalPages = 1;
+  pageSize = 10;
+  isLoading = false;
+  search = '';
+  sortBy: string = 'name';
+  sortOrder: SortOrder = 'asc';
+  constructor(
+    public popoverController: ModalController,
+    private productsService: ProductsService
+  ) {}
 
-  constructor(public popoverController: ModalController) {}
+  ngOnInit() {
+    this.loadHSNCodes();
+  }
 
-  ngOnInit() {}
+  resetPagination() {
+    this.currentPage = 1;
+    this.totalPages = 1;
+    this.pageSize = 10;
+  }
+
+  loadHSNCodes(onLoadingFinished?: () => void, isReload?: boolean) {
+    if (isReload) {
+      this.resetPagination();
+    }
+    if (this.isLoading) {
+      return;
+    }
+    this.isLoading = true;
+    console.log(1);
+    this.productsService
+      .getHSNCodes(this.itemType, {
+        page: this.currentPage.toString(),
+        pageSize: this.pageSize.toString(),
+        sortBy: this.sortBy,
+        sortOrder: this.sortOrder,
+        search: this.newItem,
+      })
+      .subscribe(
+        (response) => {
+          console.log(2);
+
+          //@ts-ignore
+          if (response.message === 'Success') {
+            //@ts-ignore
+            console.log(response.body.hsnCodes);
+            //@ts-ignore
+            this.hsnCodes = !isReload
+              ? //@ts-ignore
+                [...this.hsnCodes, ...response.body.hsnCodes]
+              : //@ts-ignore
+
+                [...response.body.hsnCodes];
+            //@ts-ignore
+            const pagination = response.body.pagination;
+            this.currentPage = pagination.page;
+            this.pageSize = pagination.pageSize;
+            this.totalPages = pagination.totalPages;
+            console.log('this.hsnCodes', this.hsnCodes);
+          }
+        },
+        (error) => {},
+        () => {
+          this.isLoading = false;
+          onLoadingFinished && onLoadingFinished();
+        }
+      );
+    console.log(3);
+  }
 
   onClosePopover = () => {
     this.popoverController.dismiss();
   };
-  selectItem(item: string) {
+  selectItem(item: HSNSACCodeI) {
     this.popoverController.dismiss({ selectedValue: item });
   }
 
   addItem() {
     if (this.newItem) {
-      this.itemList.push(this.newItem);
+      // this.itemList.push(this.newItem);
       this.popoverController.dismiss({
         selectedValue: `GST @ ${this.newItem} %`,
       });
     }
   }
+
+  loadMoreData(event: any) {
+    console.log('daa', event);
+    if (event) {
+      this.currentPage = this.currentPage + 1;
+      this.loadHSNCodes(() => event.target.complete());
+    }
+  }
+}
+
+export interface HSNSACCodeI {
+  _id: string;
+  code: string;
+  description: string;
 }
