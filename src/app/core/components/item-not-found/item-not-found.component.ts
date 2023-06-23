a763fbe1088de6089d278fa87343bfe794965f0e
Parag More <paragmore306@gmail.com>
2023-06-23 19:07:44 +0530

    basic not found

--- src/app/core/components/item-not-found/item-not-found.component.ts
+++ src/app/core/components/item-not-found/item-not-found.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-item-not-found',
   templateUrl: './item-not-found.component.html',
   styleUrls: ['./item-not-found.component.scss'],
+  imports: [IonicModule, CommonModule],
+  standalone: true,
 })
-export class ItemNotFoundComponent  implements OnInit {
-
-  constructor() { }
+export class ItemNotFoundComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
