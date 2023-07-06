98a7166195f0fe05f54e6b807b64c417a51e8f03
Parag More <paragmore306@gmail.com>
2023-07-06 19:34:08 +0530

    ui

--- src/app/core/components/invoice-template-preview/invoice-template-preview.component.ts
+++ src/app/core/components/invoice-template-preview/invoice-template-preview.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { TransactionI } from '../../services/transactions/transactions.service';
+import {
+  DiscountI,
+  TransactionI,
+} from '../../services/transactions/transactions.service';
 import { StoreInfoModel } from 'src/app/store/models/userStoreInfo.models';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -18,6 +21,51 @@ export class InvoiceTemplatePreviewComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  calculateAmount(
+    sellsPrice: number,
+    quantity: number,
+    taxIncluded: boolean | undefined,
+    gstPercentage: number | undefined,
+    cess: number | undefined,
+    discount: DiscountI | undefined | null
+  ) {
+    let amount = sellsPrice * quantity;
+    let tax = 0;
+    if (gstPercentage) {
+      tax = (amount * gstPercentage) / 100;
+    }
+    if (cess) {
+      tax = tax + (amount * cess) / 100;
+    }
+    if (!taxIncluded) {
+      amount = amount + tax;
+    }
+    if (discount) {
+      const calculatedDiscount = this.getDiscountAmount(
+        discount,
+        sellsPrice * quantity
+      );
+      amount = amount - calculatedDiscount;
+    }
+    return amount;
+  }
+
+  getDiscountAmount(discount: DiscountI, amount: number) {
+    let calculatedDiscount = 0;
+    if (discount.type === 'percentage') {
+      const newDiscount = (amount * discount.value) / 100;
+      if (discount.maxDiscount && newDiscount > discount.maxDiscount) {
+        calculatedDiscount = discount.maxDiscount;
+      } else {
+        calculatedDiscount = newDiscount;
+      }
+    }
+    if (discount.type === 'amount') {
+      calculatedDiscount = discount.value;
+    }
+    return calculatedDiscount;
+  }
 }
 
 export interface InvoiceInfoI {
