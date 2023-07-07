b9297c4b668ef24d7747f1edae3e5ccc502a6f10
Parag More <paragmore306@gmail.com>
2023-07-07 15:21:59 +0530

    invoice template ui / bill support

--- src/app/core/components/invoice-template-preview/invoice-template-preview.component.ts
+++ src/app/core/components/invoice-template-preview/invoice-template-preview.component.ts
@@ -3,24 +3,39 @@ import { IonicModule } from '@ionic/angular';
 import {
   DiscountI,
   TransactionI,
+  TransactionTypeEnum,
 } from '../../services/transactions/transactions.service';
 import { StoreInfoModel } from 'src/app/store/models/userStoreInfo.models';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DateFormatPipe } from '../../pipes/date-format.pipe';
+import { NumberToWordsPipe } from '../../pipes/number-to-words.pipe';
 
 @Component({
   selector: 'app-invoice-template-preview',
   templateUrl: './invoice-template-preview.component.html',
   styleUrls: ['./invoice-template-preview.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DateFormatPipe,
+    NumberToWordsPipe,
+  ],
 })
 export class InvoiceTemplatePreviewComponent implements OnInit {
   @Input() invoiceInfo!: InvoiceInfoI;
+  TransactionTypeEnum = TransactionTypeEnum;
   trsas = ['asdas', 'asdasd'];
   constructor() {}
 
   ngOnInit() {}
+
+  getGSTFormatString(gst: number) {
+    return '' + gst / 2 + '( ' + gst / 2 + '% )';
+  }
 
   calculateAmount(
     sellsPrice: number,
