89bf94afb915b637965a96ea2b3206928fadc780
Parag More <paragmore306@gmail.com>
2023-07-08 16:39:38 +0530

    payment creatiom

--- src/app/pages/transactions/payment-creation-form/payment-creation-form.component.ts
+++ src/app/pages/transactions/payment-creation-form/payment-creation-form.component.ts
@@ -1,14 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-payment-creation-form',
   templateUrl: './payment-creation-form.component.html',
   styleUrls: ['./payment-creation-form.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
 })
-export class PaymentCreationFormComponent  implements OnInit {
-
-  constructor() { }
+export class PaymentCreationFormComponent implements OnInit {
+  constructor() {}
 
   ngOnInit() {}
-
 }
