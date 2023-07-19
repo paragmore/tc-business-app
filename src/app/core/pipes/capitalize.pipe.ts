28d12891a07e47e8aa13c3a46f8149e45d7aab30
Parag More <paragmore306@gmail.com>
2023-07-19 14:00:39 +0530

    captialze pipe

---
+++ src/app/core/pipes/capitalize.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'capitalizeWords',
+  standalone: true,
+})
+export class CapitalizeWordsPipe implements PipeTransform {
+  transform(value: string): string {
+    if (!value) return '';
+
+    return value
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+}
