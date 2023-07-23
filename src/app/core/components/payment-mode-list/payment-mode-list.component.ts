d89ee2143eccc8ea6bd9f3f6922546b9f1b2c7ab
Parag More <paragmore306@gmail.com>
2023-07-23 13:47:16 +0530

    payment modes list

--- src/app/core/components/payment-mode-list/payment-mode-list.component.ts
+++ src/app/core/components/payment-mode-list/payment-mode-list.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-payment-mode-list',
   templateUrl: './payment-mode-list.component.html',
   styleUrls: ['./payment-mode-list.component.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule],
 })
-export class PaymentModeListComponent  implements OnInit {
+export class PaymentModeListComponent implements OnInit {
+  @Output() onSelect: EventEmitter<PaymentModesList> =
+    new EventEmitter<PaymentModesList>();
+  paymentModesList = Object.values(PaymentModesList);
+  constructor() {}
 
-  constructor() { }
+  onSelectEvent(event: PaymentModesList) {
+    if (event) {
+      this.onSelect.emit(event);
+    }
+  }
 
   ngOnInit() {}
+}
 
+export enum PaymentModesList {
+  BANK_REMITTANCE = 'Bank Remittance',
+  BANK_TRANSFER = 'Bank Transfer',
+  CASH = 'Cash',
+  CHECK = 'Check',
+  CREDIT_CARD = 'Credit Card',
 }
